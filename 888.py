d51a884fb6183071863018cfe20f1b1a7661a1a2
shiinaP <cl.cortesb@duocuc.cl>
2024-06-12 11:13:09 -0400

    Add files via upload

---
+++ 888.py
@@ -0,0 +1,13 @@
+while True:
+    
+    print ("        menu")
+    print ("")
+    print ("1-. sumar")
+    print ("2-. restar")
+    print ("3-. ")
+    print ("4-. Eliminar auto")
+    print ("5-. Modificar auto")
+    print ("6.- Generar archivo csv")
+    print ("7.- Cargar datos desde csv")
+    print ("0-. Salir")
+    break
