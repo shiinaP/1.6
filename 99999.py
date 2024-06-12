96e4ab9ed86a190eb46fa73473ab7b60e7d689bd
shiinaP <cl.cortesb@duocuc.cl>
2024-06-12 11:19:40 -0400

    Add files via upload

---
+++ 99999.py
@@ -0,0 +1,15 @@
+while True:
+    
+    print ("        menu")
+    print ("")
+    print ("1-. sumar")
+    print ("2-. restar")
+    print ("3-. ")
+    print ("4-. Eliminar auto")
+    print ("5-. Modificar auto")
+    print ("6.- Generar archivo csv")
+    print ("7.- Cargar datos desde csv")
+    print ("0-. Salir")
+    op=int(input("ingrese una opcion")
+    if op==1:
+           print("dddd")
